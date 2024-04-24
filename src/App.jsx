d849b5476feb21d5940e3474d5b3b903975a2c97
Raphael Lima <raphaellima365@gmail.com>
2024-04-24 12:32:41 -0300

    atualizacao 1

--- src/App.jsx
+++ src/App.jsx
@@ -18,52 +18,55 @@ function App() {
       <h1>Calculadora de IMC</h1>
       <form className='form'>
         <label htmlFor="peso">Peso: </label>
-        <input type="number" name="peso" value={peso} id="peso" onChange={(e) => { setPeso(e.target.value) }} /> <br />
+        <input className='form-control' type="number" name="peso" value={peso} id="peso" onChange={(e) => { setPeso(e.target.value) }} /> <br />
 
         <label htmlFor="altura">Altura: </label>
-        <input type="number" name="altura" value={altura} id="altura" onChange={(e) => { setAltura(e.target.value) }} /> <br />
+        <input className='form-control' type="number" name="altura" value={altura} id="altura" onChange={(e) => { setAltura(e.target.value) }} /> <br />
 
-        <input className='btn' type='button' value='Calcular IMC' onClick={calcular} /> <br />
+        <input className='btn btn-primary' type='button' value='Calcular IMC' onClick={calcular} /> <br />
 
         <b>Seu IMC: </b> {resultado.toFixed(2)}
 
       </form> <br />
 
-      <table border='1' style={{ borderCollapse: 'collapse' }}>
+      <table className="table table-bordered table-striped">
         <thead>
           <tr>
-            <th className='classe'>
-              Classificação
-            </th>
-            <th className='imc'>
-              IMC
-            </th>
+            <th scope="col">#</th>
+            <th scope="col">Classificação</th>
+            <th scope="col">IMC</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className='classe'>Abaixo do Peso</td>
-            <td className='imc'>Abaixo de 18,5</td>
+            <th scope="row">1</th>
+            <td>Abaixo do Peso</td>
+            <td>Abaixo de 18,5</td>
           </tr>
           <tr>
-            <td className='alt classe'>Peso Normal</td>
-            <td className='alt imc'>Entre 18,5 e 24,9</td>
+            <th scope="row">2</th>
+            <td>Peso Normal</td>
+            <td>Entre 18,5 e 24,9</td>
           </tr>
           <tr>
-            <td className='classe'>Sobre Peso</td>
-            <td className='imc'>Entre 25 e 29,9</td>
+            <th scope="row">3</th>
+            <td colSpan="1">Sobre Peso</td>
+            <td>Entre 25 e 29,9</td>
           </tr>
           <tr>
-            <td className='alt classe'>Obesidade Grau I</td>
-            <td className='alt imc'>Entre 30 e 34,9</td>
+            <th scope="row">4</th>
+            <td colSpan="1">Obesidade Grau I</td>
+            <td>Entre 30 e 34,9 </td>
           </tr>
           <tr>
-            <td className='classe'>Obesidade Grau II</td>
-            <td className='imc'>Entre 35 e 39,9</td>
+            <th scope="row">5</th>
+            <td colSpan="1">Obesidade Grau II</td>
+            <td>Entre 35 e 39,9</td>
           </tr>
           <tr>
-            <td className='alt classe'>Obesidade Grau III ou Mórbida</td>
-            <td className='alt imc'>Maior que 40</td>
+            <th scope="row">6</th>
+            <td colSpan="1">Obesidade Grau III ou Mórbida</td>
+            <td>Maior que 40</td>
           </tr>
         </tbody>
       </table>
